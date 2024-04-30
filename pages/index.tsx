d87e7cf56e7fe4d0955236b4d95001f2f35f155e
warrensgit <155303256+warrensgit@users.noreply.github.com>
2024-04-30 20:48:35 +0000

    updated index.tsx with labels

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Next.js on Replit!</h1>
+        <h1 className={styles.title}>Website Chatbot</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
