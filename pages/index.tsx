79140a666697ff07b0234ff5d6e6907998faf069
warrensgit <155303256+warrensgit@users.noreply.github.com>
2024-05-05 19:51:13 +0000

    updated index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Website Chatbot</h1>
+        <h1 className={styles.title}>AI Website Chatbot</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
